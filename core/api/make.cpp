4091281222a74a2527bd4522f7c4d0ac7014f88e
turuslan <turuslan.devbox@gmail.com>
2020-10-03 13:44:14 +0300

    tmp 9
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- core/api/make.cpp
+++ core/api/make.cpp
@@ -750,8 +750,9 @@ namespace fc::api {
                                    auto sector_number,
                                    auto tipset_key)
                                    -> outcome::result<SectorOnChainInfo> {
-          assert(false); // TODO(artyom-yurin): FIL-165 implement method
-          return outcome::success();
+            OUTCOME_TRY(context, tipsetContext(tipset_key));
+            OUTCOME_TRY(state, context.minerState(address));
+            return state.sectors.get(sector_number);
         }},
         .StateSectorPartition = {[=](auto address,
                                      auto sector_number,
